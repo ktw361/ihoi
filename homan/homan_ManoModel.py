d32a4a39efbb0b3cb50385e0f0b4f4a2daa6c3ec
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-06-15 12:30:13 +0100

    move obj_pose/utils to homan/utils

---
+++ homan/homan_ManoModel.py
@@ -0,0 +1,56 @@
+""" Implement homan's ManoModel using manopth, instead of Mano """
+
+import torch
+import torch.nn as nn
+from manopth.manolayer import ManoLayer
+
+class HomanManoModel(nn.Module):
+    
+    def __init__(self, mano_root, pca_comps=16, batch_size=1):
+        super().__init__()
+        assert pca_comps == 16
+        self.mano_layer = ManoLayer(
+            flat_hand_mean=False, 
+            ncomps=pca_comps, 
+            side='right',
+            mano_root=mano_root,
+            use_pca=True)
+
+    def forward_pca(self,
+                    pca_pose=None,
+                    rot=None,
+                    betas=None,
+                    side='right'):
+        """ 
+        Args:
+            pca_pose: (?, pca_comps+) torch.Tensor
+            rot: (1, 3) torch.Tensor
+            betas: (1, 1) torch.Tensor
+
+        Returns:
+            mano_res: dict
+                - verts: (batch_size, 778, 3)
+                - joints: (batch_size, 16, 3)
+        """
+        flat_hand_mean = False
+
+
+        if side == 'right':
+            pass
+        elif side == 'left':
+            raise NotImplementedError
+        else:
+            raise ValueError(f"{side} not in [left|right]")
+        
+        th_pose_coeffs = torch.cat([rot, pca_pose], axis=-1)
+        v, j = self.mano_layer.forward(th_pose_coeffs, betas, th_trans=None)
+        v /= 1000
+        j /= 1000
+        j_indices = [0, 5, 6, 7, 9, 10, 11, 17, 18, 19, 13, 14, 15, 1, 2, 3]
+        j_ret = j[:, j_indices, :]
+
+        mano_res = dict(
+            verts=v,
+            joints=j_ret,
+        )
+        return mano_res
---
+++ homan/homan_runner.py
@@ -0,0 +1,5 @@
+
+class HomanRunner:
+    
+    def __init__(self):
+        pass
