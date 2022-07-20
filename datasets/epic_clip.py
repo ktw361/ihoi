461e1fa67aa7c926a65833f4327ebfe566ff5a69
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-07-20 15:10:22 +0100

    update epic_clip to use cached mask boxes

--- datasets/epic_clip.py
+++ datasets/epic_clip.py
@@ -8,7 +8,6 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 from datasets.epic_lib.epic_utils import read_epic_image
-from datasets.epic_lib import epichoa
 
 
 HAND_MASK_KEEP_EXPAND = 0.2
@@ -44,14 +43,28 @@ class ClipInfo(NamedTuple):
 
 class EpicClipDataset(Dataset):
 
-    VALID = [2, 3, 4, 5, 6, 7, 10, 11, 12, 14, 16, 17, 20, 21, 22, 26, 28, 29, 31, 33, 36, 37, \
-        38, 45, 46, 47, 48, 51, 52, 53, 55, 59, 60, 63, 64, 65, 66, 67, 70, 71, 74, 75, 76, 77, \
-            78, 79, 82, 83, 85, 86, 87, 88, 89, 90, 93, 94]
+    wrong_set = {
+        # Hand box missing/wrong-side in hoa
+        ('P04_13', 10440),
+        ('P11_16', 18079),
+        ('P12_04', 1828),
+        ('P12_101', 21783),
+        ('P15_02', 25465),
+        ('P22_107', 6292),
+        ('P28_109', 10026),
+        ('P37_101', 70106),
+        # object box no merged
+        ('P01_103', 538),
+        ('P03_04', 43470),
+        ('P11_16', 16029),
+        ('P37_101', 15996),
+    }
 
     def __init__(self,
                  image_sets='/home/skynet/Zhifan/data/epic_analysis/gt_clips.json',
                  epic_root='/home/skynet/Zhifan/datasets/epic',
                  mask_dir='/home/skynet/Zhifan/data/epic_analysis/InterpV2',
+                 all_boxes='/home/skynet/Zhifan/data/epic_analysis/clip_boxes.pkl',
                  image_size=(1280, 720), # (640, 360),
                  crop_hand_mask=True,
                  *args,
@@ -67,7 +80,7 @@ class EpicClipDataset(Dataset):
             crop_hand_mask: If True, will crop hand mask with only pixels
                 inside hand_bbox.
         """
-        super(EpicClipDataset, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.epic_rgb_root = osp.join(epic_root, 'rgb_root')
         self.mask_dir = mask_dir
         self.hoa_root = osp.join(epic_root, 'hoa')
@@ -76,7 +89,8 @@ class EpicClipDataset(Dataset):
 
         self.box_scale = np.asarray(image_size * 2) / ((1920, 1080) * 2)
         self.data_infos = self._read_image_sets(image_sets)
-        self.hoa_df = dict()
+        with open(all_boxes, 'rb') as fp:
+            self.ho_boxes = pickle.load(fp)
 
     def _read_image_sets(self, image_sets):
         """
@@ -86,31 +100,19 @@ class EpicClipDataset(Dataset):
         with open(image_sets) as fp:
             infos = json.load(fp)
 
-        infos = [ClipInfo(**v) for v in infos]
+        infos = [ClipInfo(**v) 
+                 for v in infos 
+                 if (v['vid'], v['gt_frame']) not in self.wrong_set]
         return infos
 
     def __len__(self):
         return len(self.data_infos)
 
-    def _get_hand_entries(self, vid, frame_idx):
-        if vid in self.hoa_df:
-            vid_df = self.hoa_df[vid]
-        else:
-            vid_df = epichoa.load_video_hoa(vid, self.hoa_root)
-            self.hoa_df[vid] = vid_df
-        entries = vid_df[vid_df.frame == frame_idx]
-        entries = entries[entries.det_type == 'hand']
-        return entries
+    def _get_hand_box(self, vid, frame_idx, side):
+        return self.ho_boxes[vid][frame_idx][side]
 
-    def _get_obj_entries(self, vid, frame_idx):
-        if vid in self.hoa_df:
-            vid_df = self.hoa_df[vid]
-        else:
-            vid_df = epichoa.load_video_hoa(vid, self.hoa_root)
-            self.hoa_df[vid] = vid_df
-        entries = vid_df[vid_df.frame == frame_idx]
-        entries = entries[entries.det_type == 'object']
-        return entries
+    def _get_obj_box(self, vid, frame_idx, cat):
+        return self.ho_boxes[vid][frame_idx][cat]
 
     def __getitem__(self, index):
         """
@@ -135,15 +137,12 @@ class EpicClipDataset(Dataset):
         hand_masks = []
         for frame_idx in range(start, end+1):
             image = read_epic_image(
-                vid, frame_idx, root=self.epic_rgb_root, as_pil=True)
+                vid, frame_idx, as_pil=True)
             image = image.resize(self.image_size)
             image = np.asarray(image)
 
             # bboxes
-            hand_entries = self._get_hand_entries(vid, frame_idx)
-            hand_entry = hand_entries[hand_entries.side == side].iloc[0]
-            hand_box = row2xywh(hand_entry)
-            hand_box = hand_box * self.box_scale
+            hand_box = self._get_hand_box(vid, frame_idx, side)
             if side == 'right':
                 hand_bbox_dict = dict(right_hand=hand_box, left_hand=None)
             elif side == 'left':
@@ -151,12 +150,7 @@ class EpicClipDataset(Dataset):
             else:
                 raise ValueError(f"Unknown side {side}.")
 
-            obj_entries = self._get_obj_entries(vid, frame_idx)
-            if len(obj_entries) == 0:
-                obj_bbox_arr = np.empty(4)
-            else:
-                obj_entry = obj_entries.iloc[0]
-                obj_bbox_arr = (row2xywh(obj_entry) * self.box_scale).astype(np.float32)
+            obj_bbox_arr = self._get_obj_box(vid, frame_idx, cat)
 
             # masks
             path = f'{self.mask_dir}/{vid}/frame_{frame_idx:010d}.png'
@@ -216,7 +210,7 @@ class CachedEpicClipDataset(EpicClipDataset):
             crop_hand_mask: If True, will crop hand mask with only pixels
                 inside hand_bbox.
         """
-        super(CachedEpicClipDataset, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.cache_dir = 'output/epic_clip_cache'
     
     def __getitem__(self, index):
